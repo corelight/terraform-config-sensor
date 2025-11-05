83db27b4059f15c42928e2798a376be85b2b59ca
Ryan Haney <25014745+thathaneydude@users.noreply.github.com>
2025-11-05 13:33:51 -0600

    Adding Fedramp and Promethus options

--- data.tf
+++ data.tf
@@ -5,14 +5,16 @@ data "cloudinit_config" "config" {
   part {
     content_type = "text/cloud-config"
     content = templatefile("${path.module}/cloud-config/init.tpl", {
-      community_string = var.fleet_community_string
-      license          = var.sensor_license
-      mgmt_int         = var.sensor_management_interface_name
-      mon_int          = var.sensor_monitoring_interface_name
-      health_port      = var.sensor_health_check_http_port
-      probe_ranges     = var.sensor_health_check_probe_source_ranges_cidr
-      mon_subnet       = var.subnetwork_monitoring_cidr
-      mon_gateway      = var.subnetwork_monitoring_gateway
+      community_string     = var.fleet_community_string
+      license              = var.sensor_license
+      prometheus_enabled   = var.prometheus_enabled
+      fedramp_mode_enabled = var.fedramp_mode_enabled
+      mgmt_int             = var.sensor_management_interface_name
+      mon_int              = var.sensor_monitoring_interface_name
+      health_port          = var.sensor_health_check_http_port
+      probe_ranges         = var.sensor_health_check_probe_source_ranges_cidr
+      mon_subnet           = var.subnetwork_monitoring_cidr
+      mon_gateway          = var.subnetwork_monitoring_gateway
 
       fleet_token          = var.fleet_token
       fleet_url            = var.fleet_url
